a4f88f454567ff232b40683d9715025031698094
Carthew <jcarthew@MGC12YH4L5JGH6.paloalto.ford.com>
2019-10-26 17:10:13 -0400

    first run, wanna switch back fro a sec

--- chordtoy.ino
+++ chordtoy.ino
@@ -2,6 +2,7 @@
 #define MODE_SWITCH 13
 #define CHORD_POT 1
 #define INVERSION_POT 2
+#define KEY_POT 0
 #define RATE_POT 3
 #define LED_1 4
 #define LED_2 5
@@ -10,6 +11,9 @@
 #define VOICES 7 // how many notes (not including root)
 #define POLY 4
 #define INVERSION_COUNT 6//hmm
+#define THERE_ARE_12_NOTES_IN_WESTERN_MUSIC 12
+#define MIDI_ROOT_NOTE_OFFSET 9 //an octave below A0
+#define MIDI_RANGE_WITH_SCALE 81 // approx how bit our scale map array should be (72 notes in 9 octaves + the onw below)
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 int chordSelection = 0;
@@ -19,11 +23,24 @@ byte prevChord[] = {0, 0, 0};
 unsigned long strumRate = 0;
 bool trig = false;
 byte chan, vel;
-
 unsigned long currentMillis;
 unsigned long pollPrevMillis = 0;
 const long pollInterval = 100;
-
+unsigned long trigPrevMillis;
+int cur = 0; 
+int curLim = 0;
+int activeLED;
+int inactiveLED;
+int prevKey = 0;
+int key = 0; //A = 0, Bb = 1, B = 2, ...
+int keyCounter = 0;
+long keyLEDPrevMillis = 0;
+bool keyLEDStatus = false;
+// T S T T S T T
+const byte minorScaleShape[] = {2, 1, 2, 2, 1, 2, 2};
+const byte majorScaleShape[] = {2, 2, 1, 2, 2, 2, 1};
+byte scaleShape[] = {2, 1, 2, 2, 1, 2, 2};
+byte scale[MIDI_RANGE_WITH_SCALE];
 const int inversions[INVERSION_COUNT][POLY-1] = {{0, 0, 0}, {0, 0, -12}, {-12, 0, 0}, {-12, -12, 0}, {0, -12, -12}, {-12, -12, -12}};
 
 //Reface DX Polyphony
@@ -44,7 +61,7 @@ const int chords[CHORD_VARIATIONS][VOICES] = {
     {1, 1, 0, 0, 0, 1},
     {0, 1, 0, 1, 0, 1}
     };
-  
+
 const byte minorBank[] = {3, 7, 10, 14, 17, 21};
 byte bank[VOICES];
 const byte majorBank[] = {4, 7, 10, 12, 14, 17};
@@ -54,7 +71,6 @@ bool prevMode;
 void setup() {
   pinMode(LED_1, OUTPUT);
   pinMode(LED_2, OUTPUT);
-  pinMode(LED_3, OUTPUT);
   pinMode(MODE_SWITCH, INPUT);
   MIDI.setHandleNoteOn(handleNoteOn);
   MIDI.setHandleNoteOff(handleNoteOff);
@@ -63,10 +79,8 @@ void setup() {
   pollInputs();
   toggleBank();
   prevMode = modeState;
+  prevKey = key;
 }
-unsigned long trigPrevMillis;
-int cur = 0; 
-int curLim = 0;
 
 void loop() {  
   currentMillis= millis();
@@ -85,18 +99,73 @@ void loop() {
     toggleBank();
     prevMode = modeState;
   }
+  // this is just to flash the stupid LED so you know what key!
+  if (key != prevKey) {
+    buildScale();
+    keyCounter = (key + 1) * 2;
+    prevKey = key;
+    keyLEDStatus = false;
+    digitalWrite(activeLED, keyLEDStatus);
+    keyLEDPrevMillis = currentMillis + 1000;
+    
+  }
+  if ((currentMillis-keyLEDPrevMillis>=200)&&(keyCounter >= 0)) {
+    keyCounter -=1;
+    keyLEDStatus = !keyLEDStatus;
+    digitalWrite(activeLED, keyLEDStatus);
+    keyLEDPrevMillis = currentMillis;
+  }
+}
+
+int noteInScale(byte _note) {
+  int note = int(_note);
+  int start = 0;
+  if (note > 60) {
+    start = (MIDI_RANGE_WITH_SCALE/2) - 8;
+  }
+  int pos = -1;
+  for (int i = start; i < MIDI_RANGE_WITH_SCALE; i++) {
+    if (note == scale[i]) {
+      pos = i;
+      break;
+    }
+  }
+  return pos;
+}
+
+void buildScale() {
+  int root = key + MIDI_ROOT_NOTE_OFFSET;
+  scale[0] = root;
+  int j = 0;
+  for (int i = 1; i < MIDI_RANGE_WITH_SCALE; i++) {
+    scale[i] = scale[i-1] + scaleShape[j];
+    j += 1;
+    j = j % THERE_ARE_12_NOTES_IN_WESTERN_MUSIC;
+  }
+}
+
+void changeShape(byte _bank[]) {
+    for (int i = 0; i < 7; i++) {
+      scaleShape[i] = _bank[i];
+    }
 }
 
 void toggleBank() {
   if (modeState == true) {
     assignBank(minorBank);
-    digitalWrite(LED_1, HIGH);
-    digitalWrite(LED_2, LOW);
+    changeShape(minorScaleShape);
+    buildScale();
+    activeLED = LED_1;
+    inactiveLED = LED_2;
   } else {
-    digitalWrite(LED_2, HIGH);
-    digitalWrite(LED_1, LOW);
+    inactiveLED = LED_1;
+    activeLED = LED_2;
+    changeShape(majorScaleShape);
+    buildScale();
     assignBank(majorBank);
   }
+  digitalWrite(activeLED, HIGH);
+  digitalWrite(inactiveLED, LOW);
 }
 
 void pollInputs() {
@@ -110,6 +179,11 @@ void pollInputs() {
   float invSensorVal = analogRead(INVERSION_POT);
   invSensorVal = analogRead(INVERSION_POT);
   inversionSelection = int(invSensorVal * (INVERSION_COUNT/ANALOG_READ_RESOLUTION));
+  //testing key
+  inversionSelection = 0;
+  float keySensorVal = analogRead(KEY_POT);
+  keySensorVal = analogRead(KEY_POT);
+  key = int(keySensorVal * (THERE_ARE_12_NOTES_IN_WESTERN_MUSIC/ANALOG_READ_RESOLUTION));
 }
 
 void handleNoteOn(byte channel, byte pitch, byte velocity) {
@@ -122,13 +196,27 @@ void handleNoteOn(byte channel, byte pitch, byte velocity) {
   trig = true;
   // create chord to be played in here
   // first in standard shape
-  int j = 0;
-  for (int i = 0; i < VOICES; i ++ ) {
-    if (chords[chordSelection][i] == 1) {
-      curLim++;
-      thisChord[j] = bank[i] + pitch;
-      j++;
+  int pos = noteInScale(pitch);
+  pos = -1;
+  if (pos == -1) {
+    // note not in scale
+    int j = 0;
+    for (int i = 0; i < VOICES; i ++ ) {
+      if (chords[chordSelection][i] == 1) {
+        curLim++;
+        thisChord[j] = bank[i] + pitch;
+        j++;
+      }
     }
+  } else {
+    //note in scale
+    int k = 0;
+    pos += 2;
+    while (k < VOICES) {
+      thisChord[k] = scale[pos];
+      k+=1;
+      pos += 2; //triads
+    } 
   }
   // then apply inversion
   for (int k = 0; k < curLim; k++) {
